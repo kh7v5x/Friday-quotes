b9a939560edcbd191d6cc241961ac11b73cba984
Hammad Khan <hammadkhan1905@gmail.com>
2026-02-11 15:31:54 +0530

    Add files via upload

---
+++ fridayquotes.py
@@ -0,0 +1,23 @@
+import smtplib
+import datetime as dt
+import random
+
+MY_EMAIL = "example@gmail.com"
+MY_PASSWORD = 'qWeRtY1234'
+
+today = dt.datetime.now()
+day = today.weekday()
+if day == 2:
+    with open('fridayqoutes') as islamic_reminders:
+        all_lines = islamic_reminders.readlines()
+        quote = random.choice(all_lines)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL,MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f'Subject: friday quotes\n\n{quote}'
+        )
+
